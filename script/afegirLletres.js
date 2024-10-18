a0f270378b5ee08ae6405d3c12c0777a3625acb4
danivm-iticbcn <2023_daniel.vallespin@iticbcn.cat>
2024-10-18 19:49:44 +0200

    sense enye

--- script/afegirLletres.js
+++ script/afegirLletres.js
@@ -3,28 +3,13 @@ const posicionEnye = 14;
 
 //Afegir totes les lletres per jugar
 for (i=65; i<=90; i++){
-    if (i == 65 + posicionEnye){
-        crearEnye();
-    }
-    let lletra = String.fromCharCode(i)
+    let lletra = String.fromCharCode(i);
     let lletraBoto = document.createElement('button');
     lletraBoto.className = 'lletresBtn';
-    lletraBoto.id = `Btn${lletra}`
+    lletraBoto.id = `Btn${lletra}`;
     lletraBoto.textContent = `${lletra}`;
     lletraBoto.onclick = function(){
         jugarLletra(lletra);
     };
     lletresContainer.appendChild(lletraBoto);
-}
-
-//Funcio per afegir la lletra ñ
-function crearEnye(){
-    let enye = document.createElement('button');
-    enye.className = 'lletresBtn';
-    enye.id = `BtnÑ`
-    enye.textContent = `Ñ`;
-    enye.onclick = function(){
-        jugarLletra("Ñ");
-    };
-    lletresContainer.appendChild(enye);
 }
