ce52b227b28a43586be3ae261b4f7633941788c9
danivm-iticbcn <2023_daniel.vallespin@iticbcn.cat>
2024-11-25 20:52:28 +0100

    refactor puntuacio

--- script/penjat.js
+++ script/penjat.js
@@ -33,14 +33,25 @@ let data;
 let hasGuanyat;
 //LOGICA MULTIJUGADOR
 let torn = 0;
-let puntsMulti;
-let partidasGuanyades = [0, 0];
-let ratxa;
-let millorPuntuacioPartides = [0, 0];
+
+//Objecte per gestionar la puntuació de cada jugador
+const Puntuacio = {
+    "punts":0,
+    "partidesGuanyades": 0,
+    "ratxa": 0,
+    "millorPuntuacioPartides": 0,
+    "data": new Date(),
+    "percentageGuanyades": function(){
+        return (this.partidesGuanyades / partidesJugades * 100).toFixed(2);
+    }
+}
+
+//Array amb dos objectes per gestionar la puntuació de les partides
+const puntuacioMulti = [Object.create(Puntuacio), Object.create(Puntuacio)];
+
 //Temps
 let temps = [MAX_TEMPS, MAX_TEMPS];
 let senseTemps = false;
-
 
 //Evitem que el formulari refresqui la pagina i en cop d'aixo llenci comencarPartida
 document.getElementById('formularioHeader').addEventListener('submit', function(event) {
@@ -78,11 +89,8 @@ function iniciarJoc(){
     //iniciar joc
     enJuego = true;
     //Dades a 0
-    puntsMulti = [0, 0];
-    ratxa = [0, 0];
+    reiniciarDadesPuntuacio();
     jugadas = 0;
-    puntsStat0.textContent = 0;
-    puntsStat1.textContent = 0;
     hasGuanyat = false;
     //Comenca el que guanya
     if (torn == 0){
@@ -101,6 +109,17 @@ function iniciarJoc(){
     actualitzarEstatParaula();
     iniciarTiempo();
     reiniciarTemps();
+}
+
+//Funcio per reiniciar les dades necesaries de cada objecte de la llista de la puntuacio
+function reiniciarDadesPuntuacio(){
+    for (let i of puntuacioMulti){
+        i["punts"] = 0;
+        i["ratxa"] = 0;
+    }
+    //A mes possem a 0 els valors visibles del html
+    puntsStat0.textContent = 0;
+    puntsStat1.textContent = 0;
 }
 
 //Funcio per veure o deixar de veure la paraula
@@ -146,12 +165,12 @@ function actualitzarEstatPartida(){
         //Actualitzem partides jugades i guanyades
         partidesStat1.textContent = partidesJugades;
         partidesStat0.textContent = partidesJugades;
-        torn == 0 ? guanyandesStat0.textContent = partidasGuanyades[torn]:guanyandesStat1.textContent = partidasGuanyades[torn];
-        percentatge.textContent = ` (${(partidasGuanyades[0] / partidesJugades * 100).toFixed(2)}%)`;
-        percentatge2.textContent = ` (${(partidasGuanyades[1] / partidesJugades * 100).toFixed(2)}%)`;
+        torn == 0 ? guanyandesStat0.textContent = `(${puntuacioMulti[torn]["partidesGuanyades"]})`:guanyandesStat1.textContent = `(${puntuacioMulti[torn]["partidesGuanyades"]})`;
+        percentatge.textContent = ` ${puntuacioMulti[0]["percentageGuanyades"]()}%`;
+        percentatge2.textContent = ` ${puntuacioMulti[1]["percentageGuanyades"]()}%`;
     }else{
         //Actualitzem puntuacio actual
-        torn == 0 ? puntsStat0.textContent = puntsMulti[torn]:puntsStat1.textContent = puntsMulti[torn]
+        torn == 0 ? puntsStat0.textContent = puntuacioMulti[torn]["punts"]:puntsStat1.textContent = puntuacioMulti[torn]["punts"];
     }
 }
 
@@ -177,6 +196,7 @@ function reiniciarLletres(){
     });
 }
 
+//Funcio on comprobarem si la lletra introduida es correcta o no
 function comprobarLletra(lletra){
     let botonLletra = document.getElementById(`Btn${lletra}`);
     botonLletra.disabled = true;
@@ -193,17 +213,17 @@ function comprobarLletra(lletra){
     }
     //Si acerta sumem ratxa si no reiniciem i incrementem jugadas
     if(acertat){
-        ratxa[torn] += 1;
+        puntuacioMulti[torn]["ratxa"] += 1;
     } else{
         jugadas++;
-        imatge.src = `/img/penjat_${jugadas}.jpg`;
-        ratxa[torn] = 0;
-        if (puntsMulti[torn] > 0){
-            puntsMulti[torn]--;
+        imatge.src = `./img/penjat_${jugadas}.jpg`;
+        puntuacioMulti[torn]["ratxa"] = 0;
+        if (puntuacioMulti[torn]["punts"] > 0){
+            puntuacioMulti[torn]["punts"]--;
         }
     }
     //Actualitzem estats puntuacio i paraula
-    puntsMulti[torn] += puntsJugada * ratxa[torn];
+    puntuacioMulti[torn]["punts"] += puntsJugada * puntuacioMulti[torn]["ratxa"];
     actualitzarEstatParaula();
     actualitzarEstatPartida()
     acertat ? '':cambiarTorn();
@@ -242,7 +262,7 @@ function lancarGuanyar(){
     torn == puntuacioMajor() ? '':cambiarTorn();
     //Actualitzem dades
     partidesJugades++;
-    partidasGuanyades[torn]++;
+    puntuacioMulti[torn]["partidesGuanyades"]++;
     contenidorEstat.style.backgroundColor = 'green';
     habilitarElementsHeader();
     actualitzarEstatPartida();
@@ -254,16 +274,18 @@ function lancarGuanyar(){
 
 //Funcio per veure si la partida es millor que l'anterior
 function comprovarMillorPartida(){
-    if (puntsMulti[torn] > millorPuntuacioPartides[torn]){
-        millorPuntuacioPartides[torn] = puntsMulti[torn];
+    if (puntuacioMulti[torn]["punts"] > puntuacioMulti[torn]["millorPuntuacioPartides"]){
+        puntuacioMulti[torn]["millorPuntuacioPartides"] = puntuacioMulti[torn]["punts"];
         //Agafem la data i hora actuals
         let data = new Date().toLocaleDateString('es-ES');
         let hora = new Date().toLocaleTimeString('es-ES');
         //Segons el jugador asignem a un o un altre
         if(torn == 0){
-            millorPartidaEstat0.textContent = `${data} ${hora} - ${millorPuntuacioPartides[torn]} punts`;
+            puntuacioMulti[0]["data"] = `${data} ${hora}`;
+            millorPartidaEstat0.textContent = `${puntuacioMulti[0]["data"]} - ${puntuacioMulti[torn]["millorPuntuacioPartides"]} punts`;
         }else{
-            millorPartidaEstat1.textContent = `${data} ${hora} - ${millorPuntuacioPartides[torn]} punts`;
+            puntuacioMulti[1]["data"] = `${data} ${hora}`;
+            millorPartidaEstat1.textContent = `${puntuacioMulti[1]["data"]} - ${puntuacioMulti[torn]["millorPuntuacioPartides"]} punts`;
         }
     }
 }
@@ -286,11 +308,11 @@ function cambiarTorn(){
 //Funcio per saber que te més puntuacio i qui guanya
 function puntuacioMajor(){
     let ganador = 0;
-    let puntuacioMesGran = puntsMulti[0];
-    if (puntuacioMesGran < puntsMulti[1]){
-        puntuacioMesGran=puntsMulti[1];
+    let puntuacioMesGran = puntuacioMulti[0]["punts"];
+    if (puntuacioMesGran < puntuacioMulti[1]["punts"]){
+        puntuacioMesGran=puntuacioMulti[1]["punts"];
         ganador = 1;
-    } else if(puntuacioMesGran == puntsMulti[1]){
+    } else if(puntuacioMesGran == puntuacioMulti[1]["punts"]){
         ganador = torn;
     }
     return ganador
@@ -303,6 +325,7 @@ function reiniciarTemps(){
     document.getElementById('temps1').textContent = temps[torn] + 's';
 }
 
+//Funcio per tenir comptador de temps per a cada jugador
 function iniciarTiempo(){
     const intervalo = setInterval(() => {
         //Netejem l'interval
